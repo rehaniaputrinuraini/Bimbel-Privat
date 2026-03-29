03e320af8cf860515258c5284a700f83aef66a30
rehaniaputrinuraini <rehania2018putri@gmail.com>
2026-03-29 11:47:54 +0700

    Menambahkan fitur company profile

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 use App\Http\Controllers\MuridController;
 
 Route::resource('murid', MuridController::class);
+
+use App\Http\Controllers\LandingPageController;
+
+Route::get('/', [LandingPageController::class, 'index'])->name('landing');
+
+Route::get('/login', function () {
+    return redirect('/');
+})->name('login');
 
 use App\Http\Controllers\PaketController;
 
